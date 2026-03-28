3c758bcaa5ea7ca83e9544b2689457b7278ab7be
Andrija Adamovic <andrija.adamov@gmail.com>
2026-03-28 18:05:21 +0100

    modified:   .gitignore 	new file:   matlab/linear_system_example_1.m 	new file:   tex/bachelors_thesis.aux 	new file:   tex/bachelors_thesis.fdb_latexmk 	new file:   tex/bachelors_thesis.fls 	new file:   tex/bachelors_thesis.log 	new file:   tex/bachelors_thesis.pdf 	new file:   tex/bachelors_thesis.synctex.gz 	new file:   tex/bachelors_thesis.tex

---
+++ matlab/linear_system_example_1.m
@@ -0,0 +1,33 @@
+% Dual mass spring damper system
+
+%Masses [kg]
+m1 = 1.0;
+m2 = 0.8;
+
+%Spring constants [N/m]
+k1 = 120;
+k2 = 180;
+
+%Damping coefficients [Ns/m]
+c1 = 1.5;
+c2 = 2.0;
+
+% Continuous LTI State-space matrices
+A = [0, 1, 0, 0; 
+     -(k1+k2)/m1, -(c1 + c2)/m1, k2/m1, c2/m1; 
+     0, 0, 0, 1; 
+     k2/m2, c2/m2, -k2/m2, -c2/m2];
+
+B = [0; 0; 0; 1/m2];
+
+C = [1, 0, 0, 0; 
+     0, 0, 1, 0];
+
+D = [0; 0]; % No feedforward
+
+T = 1e-2; % Sampling time
+
+sys_c = ss(A, B, C, D);
+sys_d = c2d(sys_c, T, 'zoh');
+
+step(sys_d);
