102e316afa721630856e2a9947f5ef07712885f1
Andrija Adamovic <andrija.adamov@gmail.com>
2026-03-29 17:16:19 +0200

    modified:   matlab/create_hankel.m

--- matlab/create_hankel.m
+++ matlab/create_hankel.m
@@ -1,9 +1,3 @@
-% function H = create_hankel(data, depth)
-%     H = zeros(depth, length(data) - depth + 1);
-%     for k = 1:depth
-%         H(k, :) = data(k : (length(data) - depth + k), :);
-%     end
-% end
 function H = create_hankel(data, depth)
     if isvector(data)
         data = data(:);
