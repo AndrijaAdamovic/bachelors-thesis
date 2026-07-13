9e1926ce129c2142a362d6d453d975dd3feb489c
Andrija Adamovic <andrija.adamov@gmail.com>
2026-07-13 21:25:08 +0200

    modified:   matlab/deepc_nonlinear_example.m 	modified:   tex/bachelors_thesis.aux 	modified:   tex/bachelors_thesis.fdb_latexmk 	modified:   tex/bachelors_thesis.lof 	modified:   tex/bachelors_thesis.log 	modified:   tex/bachelors_thesis.pdf 	modified:   tex/bachelors_thesis.synctex.gz 	modified:   tex/bachelors_thesis.tex

--- matlab/deepc_nonlinear_example.m
+++ matlab/deepc_nonlinear_example.m
@@ -5,22 +5,22 @@ C = [0 0 1 0];
 k23 = 24;
 
 Ts = 0.01;
-u = 100;
+u = 72.59;
 x = zeros(4, 1);
 x_l = zeros(4, 1);
 
-y = zeros(100, 1);
-y_l = zeros(100, 1);
+y = zeros(2000, 1);
+y_l = zeros(2000, 1);
 
-for i = 1:100
+for i = 1:2000
     y(i) = C*x;
     y_l(i) = C*x_l;
     x = nonlinear_double_smd(x, u, Ts, A, B, m1, m2, k23);
     x_l = nonlinear_double_smd(x_l, u, Ts, A, B, m1, m2, 0);
 end
 
-plot((1:100)*Ts, y_l);
+plot((1:2000)*Ts, y_l);
 hold on;
-plot((1:100)*Ts, y);
+plot((1:2000)*Ts, y);
 hold off;
 legend("Linear", "Nonlinear");
